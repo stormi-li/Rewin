5997334faffed74e67f30075c039712ff501fad2
Your Name <you@example.com>
2024-10-22 22:06:55 +0800

    v1.0.1

--- Nodebuilder.go
+++ Nodebuilder.go
@@ -10,24 +10,27 @@ import (
 
 func CreateRedisNode(port int, path string) {
 	os.MkdirAll(path, 0755)
-	TruncateFile(path + "/redis.conf")
-	AppendToFile(path+"/redis.conf", "bind 0.0.0.0\n")
-	AppendToFile(path+"/redis.conf", "protected-mode no\n")
-	AppendToFile(path+"/redis.conf", "databases 1\n")
-	AppendToFile(path+"/redis.conf", "daemonize yes\n")
-	AppendToFile(path+"/redis.conf", "port "+strconv.Itoa(port)+"\n")
-	AppendToFile(path+"/redis.conf", "dir "+path+"\n")
-	AppendToFile(path+"/redis.conf", "always-show-logo yes\n")
-	AppendToFile(path+"/redis.conf", "loglevel verbose\n")
-	AppendToFile(path+"/redis.conf", "save 900 1\n")
-	AppendToFile(path+"/redis.conf", "save 300 10\n")
-	AppendToFile(path+"/redis.conf", "save 60 10000\n")
+	//删除配置文件
+	truncateFile(path + "/redis.conf")
+	//创建配置文件
+	appendToFile(path+"/redis.conf", "bind 0.0.0.0\n")
+	appendToFile(path+"/redis.conf", "protected-mode no\n")
+	appendToFile(path+"/redis.conf", "databases 1\n")
+	appendToFile(path+"/redis.conf", "daemonize yes\n")
+	appendToFile(path+"/redis.conf", "port "+strconv.Itoa(port)+"\n")
+	appendToFile(path+"/redis.conf", "dir "+path+"\n")
+	appendToFile(path+"/redis.conf", "always-show-logo yes\n")
+	appendToFile(path+"/redis.conf", "loglevel verbose\n")
+	appendToFile(path+"/redis.conf", "save 900 1\n")
+	appendToFile(path+"/redis.conf", "save 300 10\n")
+	appendToFile(path+"/redis.conf", "save 60 10000\n")
+	//启动redis
 	go func() { exec.Command("cmd", "/C", "start redis-server "+path+"/redis.conf").CombinedOutput() }()
 	time.Sleep(100 * time.Millisecond)
 }
 
-func TruncateFile(filename string) {
-	CreateFileNX(filename)
+func truncateFile(filename string) {
+	createFileNX(filename)
 	file, _ := os.OpenFile(filename, os.O_RDWR, 0644)
 
 	defer file.Close()
@@ -35,7 +38,7 @@ func TruncateFile(filename string) {
 	file.Truncate(0)
 }
 
-func CreateFileNX(filename string) {
+func createFileNX(filename string) {
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		file, err := os.Create(filename)
 		if err != nil {
@@ -45,7 +48,7 @@ func CreateFileNX(filename string) {
 	}
 }
 
-func AppendToFile(filename string, s string) {
+func appendToFile(filename string, s string) {
 	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return
